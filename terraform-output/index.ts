126f15f903f15fd3f95d8f3bc011f4a6a774808d
Richard Simpson <rsimpson@uship.com>
2021-03-10 17:11:39 -0600

    stuff and things

--- terraform-output/index.ts
+++ terraform-output/index.ts
@@ -2,6 +2,9 @@ import { getInput, setFailed } from "@actions/core";
 
 async function run() {
   try {
+    for (const [key, val] of Object.entries(process.env)) {
+      console.log(`${key}=${val}`);
+    }
     console.log("Got Steps", getInput("steps"));
   } catch (e) {
     setFailed(e);
