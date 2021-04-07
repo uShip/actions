437e89c832706ad9ca0b2a4abca9a04e85364770
Richard Simpson <rsimpson@uship.com>
2021-04-07 17:25:53 -0500

    chore: limit lint for some files to just workspaces

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,14 @@
+const { relative } = require("path");
+const packageJson = require('./package.json');
+
 module.exports = {
-  "**/*.{ts,js,json}": (files) => ["npm run build", "git add **/dist/**"],
-  "*.{ts,yml}": ["prettier --write"]
+  "**/*": (files) =>
+    files
+      .map((file) => relative(process.cwd(), file))
+      .some((file) =>
+        packageJson.workspaces.some(workspace => file.startsWith(workspace))
+      )
+      ? ["npm run build", "git add **/dist/**"]
+      : [],
+  "*.{ts,yml}": ["prettier --write"],
 };
